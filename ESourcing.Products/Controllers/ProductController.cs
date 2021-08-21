f48a77130544254c43a748925bf7915de6b8b111
omeru <omeru@DESKTOP-I9VC297>
2021-08-21 23:21:00 +0300

    Add project files.

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
